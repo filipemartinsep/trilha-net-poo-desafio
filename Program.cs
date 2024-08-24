c83427a1916e5b6fede02a3d14992ad161e718ca
Filipe Martins Pinto <filipemartinsep@gmail.com>
2024-08-24 19:28:57 -0300

    feat: add final features

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12344", modelo: "C210", imei: "001970-01-010000-0", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("TikTok");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "12315", modelo: "A2848", imei: "001970-01-010000-1", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
